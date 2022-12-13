59cd6472773a0cf11e4973ab450fdf70545fa64a
Ramanathan Arunachalam <rarunachalam@nvidia.com>
2022-12-13 13:56:21 +0530

    Fix pip eff package link for tao-deploy

--- tao_deploy/setup_env_colab.sh
+++ tao_deploy/setup_env_colab.sh
@@ -21,7 +21,7 @@ python3.8 -m pip install nvidia-pyindex
 python3.8 -m pip install --upgrade setuptools
 python3.8 -m pip install pycuda==2020.1
 python3.8 -m pip install https://files.pythonhosted.org/packages/d1/c2/c14dd8884a5bc05ca07331b3d78a92812eb19e25a625a0b59af8b609a93f/nvidia_eff_tao_encryption-0.1.7-cp38-cp38-manylinux_2_17_x86_64.manylinux2014_x86_64.whl
-python3.8 -m pip install nvidia-eff==0.6.2
+python3.8 -m pip install https://files.pythonhosted.org/packages/cf/ec/47f770919111bcd7047e463389e7f763afbc6ae7b96cbd4be974342a5bb1/nvidia_eff-0.6.2-py38-none-manylinux_2_17_x86_64.manylinux2014_x86_64.whl
 python3.8 -m pip install cffi
 
 #Install TensorRT whl files
